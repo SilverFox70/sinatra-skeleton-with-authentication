7f22efe1e013762a7d2083e2ac29bb24a4e198b4
Bill Brinkert <email": null>
2016-01-15 11:31:02 -0800

    Update seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@ people = [{first_name: "Bill", last_name: "Brinkert", email: "wfbrinkert@gmail.c
  {first_name: "Sue", last_name: "Dahner", email: "sdahner@gmail.com", age: 41, gender: "female", role: 0, password: "abcd"},
  {first_name: "Joy", last_name: "Kessler", email: "jkes@gmail.com", age: 42, gender: "female", role: 0, password: "qwer"}]
 
- friends = [{user_id: 1, user_id: 2, relationship_code: 0},
-            {user_id: 1, user_id: 3, relationship_code: 1},
-            {user_id: 1, user_id: 4, relationship_code: 2},
-            {user_id: 2, user_id: 3, relationship_code: 2},
-            {user_id: 2, user_id: 4, relationship_code: 1}]
+ friends = [{user_id: 1, target_user_id: 2, relationship_code: 0},
+            {user_id: 1, target_user_id: 3, relationship_code: 1},
+            {user_id: 1, target_user_id: 4, relationship_code: 2},
+            {user_id: 2, target_user_id: 3, relationship_code: 2},
+            {user_id: 2, target_user_id: 4, relationship_code: 1}]
 
 people.each do |person|
   User.create(person)
